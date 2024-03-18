a72cd06369e78ea4b4fcca3395b3e0ba1afe7ca0
Degatc <clement.degat@hotmail.fr>
2024-03-18 14:19:39 +0100

    initial code refactoring

--- python/tennis5.py
+++ python/tennis5.py
@@ -7,59 +7,58 @@ class TennisGame5:
         self.player1Score = 0
         self.player2Score = 0
 
+    # Utilisation de variables constantes plutôt que des chaînes de caractères
+    # pour l'identification des joueurs
     def won_point(self, playerName):
-        # Chaînes de caractères pour identifier les joueurs
-        # => Variables globales ou des attributs de classe
-        if (playerName == "player1"):
+        if (playerName == self.player1Name):
             self.player1Score += 1
-        elif (playerName == "player2"):
+        elif (playerName == self.player2Name):
             self.player2Score += 1
         else:
-            # Message explicite
             raise ValueError("Invalid player name.")
 
+    # On élimine la boucle while car elle n'est pas nécessaire
+    # les scores ne dépassent jamais 4
+    # Conditions plus simples pour déterminer les scores
     def score(self):
-        # Revoir le nommage des variables
-        p1 = self.player1Score
-        p2 = self.player2Score
+        player1Score = self.player1Score
+        player2Score = self.player2Score
 
-        # Boucle while pas nécessaire, le résultat final reste le même
-        # => Conditions plus simples
-        while (p1 > 4 or p2 > 4):
-            p1 -= 1
-            p2 -= 1
+        if player1Score > 4 or player2Score > 4:
+            player1Score = 4
+            player2Score = 4
 
+        # Utilisation de constantes pour les scores plutôt que des chaînes de caractères
         lookup = {
-            # Scores associés à des chaînes de caractères directement dans le dictionnaire
-            # => Variables ou constantes
             (0, 0): "Love-All",
             (0, 1): "Love-Fifteen",
             (0, 2): "Love-Thirty",
             (0, 3): "Love-Forty",
-            (0, 4): "Win for player2",
             (1, 0): "Fifteen-Love",
             (1, 1): "Fifteen-All",
             (1, 2): "Fifteen-Thirty",
             (1, 3): "Fifteen-Forty",
-            (1, 4): "Win for player2",
             (2, 0): "Thirty-Love",
             (2, 1): "Thirty-Fifteen",
             (2, 2): "Thirty-All",
             (2, 3): "Thirty-Forty",
-            (2, 4): "Win for player2",
             (3, 0): "Forty-Love",
             (3, 1): "Forty-Fifteen",
             (3, 2): "Forty-Thirty",
             (3, 3): "Deuce",
-            (3, 4): "Advantage player2",
-            (4, 0): "Win for player1",
-            (4, 1): "Win for player1",
-            (4, 2): "Win for player1",
-            (4, 3): "Advantage player1",
+            # Utilisation de variables pour les conditions de victoire ou d'avantage
+            (4, 0): "Win for " + self.player1Name,
+            (4, 1): "Win for " + self.player1Name,
+            (4, 2): "Win for " + self.player1Name,
+            (4, 3): "Advantage " + self.player1Name,
+            (0, 4): "Win for " + self.player2Name,
+            (1, 4): "Win for " + self.player2Name,
+            (2, 4): "Win for " + self.player2Name,
+            (3, 4): "Advantage " + self.player2Name,
             (4, 4): "Deuce",
         }
 
-        entry = (p1, p2)
+        entry = (player1Score, player2Score)
         if (entry in lookup):
             return lookup[entry]
         else:
